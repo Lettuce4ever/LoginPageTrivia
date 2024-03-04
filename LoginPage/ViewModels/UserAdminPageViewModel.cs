7d3ebd64248227973fde901c359f12db5364ab8c
User <User@LAB3-3>
2024-03-04 14:09:46 +0200

    gitpush

--- LoginPage/ViewModels/UserAdminPageViewModel.cs
+++ LoginPage/ViewModels/UserAdminPageViewModel.cs
@@ -7,21 +7,49 @@ using System.Text;
 using System.Threading.Tasks;
 using LoginPage.Service;
 using System.Collections.ObjectModel;
+using LoginPage.Service;
+using Javax.Security.Auth;
+using System.Windows.Input;
 
 
 namespace LoginPage.ViewModels
 {
-    public class UserAdminPageViewModel:ViewModel
+    public class UserAdminPageViewModel : ViewModel
     {
         public ObservableCollection<Player> Players { get; set; }//אוסף שחקנים
+        private bool isRefreshing;
+        public bool IsRefreshing { get => IsRefreshing; set => IsRefreshing = value; } //יש מצב שצריך להוסיף on property changed
+
+        public ICommand RefreshCommand { get; private set; }
 
         //צריך להוסיף פעולות
 
-        public UserAdminPageViewModel() 
+        public UserAdminPageViewModel()
         {
             Players = new ObservableCollection<Player>();
-            
+
+            RefreshCommand = new Command(async () => await Refresh());
         }
+
+        private async Task LoadPlayers()
+        {
+            LoadPlayers();
+            UserService Service = new UserService();
+            foreach (var player in Service.playersList)
+            {
+                Players.Add(player);
+
+            }
+        }
+
+        private async Task Refresh()
+        {
+            IsRefreshing = true;
+            await LoadPlayers();
+            IsRefreshing = false;
+        }
+
+ 
 
     }
 }
